5609792a399436dc664c5e8cf3eea854bfabc3c0
Nisarg-M-Patel <patel.m.nisarg@gmail.com>
2025-12-26 14:03:52 -0800

    Initial Commit

---
+++ FeedAnalyzer/FeedAnalyzerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FeedAnalyzerApp.swift
+//  FeedAnalyzer
+//
+//  Created by Nisarg Patel on 12/26/25.
+//
+
+import SwiftUI
+
+@main
+struct FeedAnalyzerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
